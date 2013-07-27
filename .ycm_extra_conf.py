c1e610e79980e02fad40ed9e5ded6bdcca94579c
ari edelkind <edelkind+git@gmail.com>
2013-07-27 00:37:14 -0400

    YouCompleteMe files

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,13 @@
+flags = [
+        '-Wall',
+        '-Werror',
+        '-DUSE_CLANG_COMPLETER',
+        '-std=c99',
+        '-x', 'c',
+    ]
+
+def FlagsForFile(fname):
+    return {
+            'flags' : flags,
+            'do_cache' : True
+        }
